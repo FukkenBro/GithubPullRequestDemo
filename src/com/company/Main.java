e3983930199c261be67fca4d4124037e538744a1
FukkenBro <stars.sun.moon@mail.ru>
2020-04-02 04:16:45 +0300

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,33 @@
-package com.company;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 public class Main {
 
+    private static final int SIZE = 100;
+    private static final int MIN = 0;
+    private static final int MAX = 50;
+
     public static void main(String[] args) {
-        // show me you Stream API power
-        System.out.println("test");
+
+        Random random = new Random();
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < SIZE; i++) {
+            list.add(Integer.toString(random.nextInt(SIZE)));
+        }
+
+        int[] array = list.stream()
+                .mapToInt(Integer::parseInt)
+                .filter((i) -> (i >= MIN && i <= MAX))
+                .distinct()
+                .sorted()
+                .toArray();
+        System.out.println("Size = " + array.length);
+        System.out.println(Arrays.toString(array));
+        double avg = list.stream().mapToInt(Integer::parseInt).average().getAsDouble();
+        System.out.println("Average is "+avg);
     }
+
 
 }
